dcb19e6b3d54892b5a22df595a0a767ca661d8f0
Diokou Tech <diokoucheikh@gmail.com>
2022-05-17 01:14:24 +0000

    update add task delete app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloword_app';
+  title = 'Todo List App';
+  langName ='';
+  langStatus='';
+  status = [
+    'learned',
+    'learn in progress',
+    'not learn',
+  ]
+  langages = [
+      {name : 'PHP', id: 0, status: 'learned'},
+      {name : 'Javascript', id: 1, status: 'learned'},
+      {name : 'Java', id:2, status: 'learn in progress'},
+      {name : 'Python', id : 3, status: 'not learn'},
+  ];
+
+  deleteLang(langId: number){
+    // this.langages.splice(langId,1);  
+    const IndexElement = this.langages.findIndex((task) => task.id === langId);
+    this.langages.splice(IndexElement,1);  
+  }
+  addLang(){
+    // alert('addLang click')
+    const lastIndex = this.langages.length - 1 ;
+    const id = lastIndex + 1;
+    this.langages.push({'name' : this.langName, 'id' : id, 'status' : this.langStatus});
+    this.langName = '';
+    this.langStatus = '';
+  }
 }
